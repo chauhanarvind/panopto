f50f3f644f5cf9136af13bd898e51843b4b421a5
arvind <chauhan.m.arvind@gmail.com>
2024-09-22 20:01:30 +1200

    cors

--- index.py
+++ index.py
@@ -4,8 +4,8 @@ import lib
 
 app = Flask(__name__)
 
-# Enable CORS for all routes
-CORS(app, resources={r"/*": {"origins": "https://panopto-nextjs.vercel.app/"}})
+# Enable CORS for all routes, including handling preflight requests (OPTIONS)
+CORS(app, resources={r"/*": {"origins": "https://panopto-nextjs.vercel.app"}})
 
 @app.route('/', methods=['POST'])
 def download_video_mp4():
